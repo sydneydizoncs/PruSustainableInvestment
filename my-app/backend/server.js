5b1e50dd7f78601ca4ca20249dbb2dd6a4eea5ae
mattcruz45 <mattcruz4545@gmail.com>
2024-06-27 08:54:59 -0400

    hope

--- my-app/backend/server.js
+++ my-app/backend/server.js
@@ -43,6 +43,6 @@ app.get('/api/stocks/:ticker', (req, res) => {
   });
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
